b1a7df1b3bb1e8d7de3eba0f420b54531a01e08b
Bilal-Abbas-Gigalabs <bilal.abbas@gigalabs.co>
2024-01-23 02:56:04 +0500

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "custom/jquery-3.4.1.min"
+import "custom/bootstrap"
+import "custom/custom"
